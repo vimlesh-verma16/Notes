7ba41ba3e59a965b78c6ba2a68a749b6f07fb98d
vimlesh <vimleshverma1998@gmail.com>
2025-03-06 21:23:13 +0530

    Django revesion level 1 and level 2

--- Django_Notes/first_project/first_project/urls.py
+++ Django_Notes/first_project/first_project/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from first_app import views
 from django.conf.urls.static import static
 
 urlpatterns = [
-    #path('',views.index,name="index"),
-    path('first_app/',include('first_app.urls')),
-    path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
+    path("first_app/", include("first_app.urls")),
+    path("admin/", admin.site.urls),
 ]
- 
