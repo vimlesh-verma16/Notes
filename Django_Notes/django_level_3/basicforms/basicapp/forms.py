a8fa8d7bafe2b2b8667d92b069270611e2df626a
vimlesh <vimleshverma1998@gmail.com>
2025-03-07 19:25:52 +0530

    django level 3

--- Django_Notes/django_level_3/basicforms/basicapp/forms.py
+++ Django_Notes/django_level_3/basicforms/basicapp/forms.py
@@ -1,28 +1,24 @@
-from django import forms 
+from django import forms
 from django.core import validators
+
 
 def check_for_z(value):
 
-    if value[0].lower() != 'z':
+    if value[0].lower() != "z":
         raise forms.ValidationError("the name Needs start with z")
 
 
 class FormName(forms.Form):
     name = forms.CharField(validators=[check_for_z])
-   
     email = forms.EmailField()
-    verify_email = forms.EmailField(label= "Enter your mail again to verify")
-    text = forms.CharField(widget = forms.Textarea)
-    detail = forms.CharField(widget = forms.Textarea)
-
+    verify_email = forms.EmailField(label="Enter your mail again to verify")
+    text = forms.CharField(widget=forms.Textarea)
+    detail = forms.CharField(widget=forms.Textarea)
 
     def clean(self):
         all_cleaned_data = super().clean()
-        email = all_cleaned_data['email']
-        vmail = all_cleaned_data['verify_email']
+        email = all_cleaned_data["email"]
+        vmail = all_cleaned_data["verify_email"]
 
         if email != vmail:
             raise forms.ValidationError("mismatch between the emails")
-
-    
-
--- Django_Notes/django_level_3/basicforms/basicapp/views.py
+++ Django_Notes/django_level_3/basicforms/basicapp/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
 from . import forms
-# Create your views here.
+
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, "index.html")
+
 
 def forms_name_view(request):
     form = forms.FormName()
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = forms.FormName(request.POST)
 
         if form.is_valid():
 
             print("Validation Succesfull")
-            print("Name"+ form.cleaned_data["name"])
-            print("Detail"+ form.cleaned_data["detail"])
-            print("EMAIL"+ form.cleaned_data["email"])
-            print("Text"+ form.cleaned_data["text"])
-    return render(request,"forms.html",{'form':form})
+            print("Name: " + form.cleaned_data["name"])
+            print("Detail: " + form.cleaned_data["detail"])
+            print("EMAIL: " + form.cleaned_data["email"])
+            print("Text: " + form.cleaned_data["text"])
+    return render(request, "forms.html", {"form": form})
