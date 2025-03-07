23f9735a1840b280b90ba34ce1e22ba1ee311d75
vimlesh <vimleshverma1998@gmail.com>
2025-03-07 19:25:10 +0530

    django level 4

--- Django_Notes/django_level_4/template_learning/basic_app/views.py
+++ Django_Notes/django_level_4/template_learning/basic_app/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
-    return render(request,'basic_app/index.html')
+    return render(request, "basic_app/index.html", {"vimlesh": "I am vimlesh"})
+
 
 def other(request):
-    return render(request,'basic_app/other.html')
+    return render(request, "basic_app/other.html")
+
 
 def relative(request):
-    return render(request,'basic_app/relative_urls_template.html')
+    return render(request, "basic_app/relative_urls_template.html")
